9b5d46427833035906e5a6ae1b70f493b55b7d8d
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-10-27 12:24:50 +0100

    trackBy ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
+import { DoCheck, ElementRef, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
-
+interface Item {
+  id: number,
+  name: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+  @ViewChild('myDivWith') theDivWith: ElementRef;
+  @ViewChild('myDivWithout') theDivWithout: ElementRef;
+  changeDetection: string = '';
   title = 'my-angular-app';
+  items: Item[] = [
+    { id: 1, name: 'Paolo' },
+    { id: 2, name: 'Marco' }
+  ]
+  trackByItems(index: number, item: Item): number { return item.id };
+  resetItems() {
+    this.items[0] = { id: 1, name: "Paolo" };
+    this.items[1] = { id: 2, name: "Marco" }
+  }
+  changeIds() {
+    this.items[0].id = 3;
+    this.items[1].id = 4;
+  }
+  add() {
+    this.items = [
+      { id: 1, name: 'Paolo' },
+      { id: 2, name: 'Marco' },
+      { id: 3, name: "Luca" }
+    ]
+  }
+  clearCounts() {
+    this.items = [
+      { id: 1, name: 'Paolo' },
+      { id: 2, name: 'Marco' }
+    ]
+    this.changeDetection = "";
+  }
+  ngDoCheck() {
+    this.changeDetection += " changed";
+  }
 }
